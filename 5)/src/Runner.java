0f88e3bbed032c4351c51b94afb8ccc1b1be4ec5
gromov-nikita <hromay.n999@gmail.com>
2021-02-08 22:19:21 +0300

    1

---
+++ 5)/src/Runner.java
@@ -0,0 +1,8 @@
+/*
+Exercise 5: (3) Modify ListFeatures.java so that it uses Integers (remember
+autoboxing!) instead of Pets, and explain any difference in results.
+
+*/
+public class Runner {
+
+}
---
+++ 7)/src/Runner.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+Exercise 7: (3) Create a class, then make an initialized array of objects of your class. Fill
+a List from your array. Create a subset of your List by using subList( ), then remove this
+subset from your List.
+
+ */
+public class Runner {
+    public static void main(String[] args) {
+        Class [] array = new Class[5];
+        array[0] = new Class();
+        array[1] = new Class();
+        array[2] = new Class();
+        array[3] = new Class();
+        array[4] = new Class();
+        List<Class> list = new ArrayList<Class>();
+        for(int i = 0; i < 5; i++) {
+            list.add(array[i]);
+        }
+        System.out.println(list.size());
+        list.subList(1,3).clear();
+        System.out.println(list.size());
+
+    }
+}
