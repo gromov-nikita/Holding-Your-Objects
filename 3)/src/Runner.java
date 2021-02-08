e2e1249296340867478ce198524fe5c271186120
gromov-nikita <hromay.n999@gmail.com>
2021-02-08 21:23:35 +0300

    1

---
+++ 3)/src/Runner.java
@@ -0,0 +1,6 @@
+/*
+Exercise 3: (2) Modify innerclasses/Sequence.java so that you can add any number
+of elements to it.
+ */
+public class Runner {
+}
---
+++ 3)/src/Sequence.java
@@ -0,0 +1,28 @@
+import java.util.Vector;
+
+public class Sequence {
+    private Vector<Object> items;
+    public Sequence(int size) { items = new Vector<Object>(); }
+    public void add(Object x) {
+        items.add(x);
+    }
+    private class SequenceSelector implements Selector {
+        private int i = 0;
+        public boolean end() { return i == items.size(); }
+        public Object current() { return items.get(i); }
+        public void next() { if(i < items.size()) i++; }
+    }
+    public Selector selector() {
+        return new SequenceSelector();
+    }
+    public static void main(String[] args) {
+        Sequence sequence = new Sequence(10);
+        for(int i = 0; i < 10; i++)
+            sequence.add(Integer.toString(i));
+        Selector selector = sequence.selector();
+        while(!selector.end()) {
+            System.out.print(selector.current() + " ");
+            selector.next();
+        }
+    }
+}
