402e82d68397838d83a63486985ba41970c5a3f4
gromov-nikita <hromay.n999@gmail.com>
2021-03-11 13:28:32 +0300

    1

--- 14)/src/Runner.java
+++ 14)/src/Runner.java
@@ -8,7 +8,7 @@ public class Runner {
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<Integer>();
         list.add(-1);
-        list.addLast(-2);
+        list.add(-2);
         ListIterator<Integer> lit = list.listIterator(1);
         for(int i = 0; i < 10; i++) {
             lit.add(i);
